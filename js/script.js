55d79bc9541c978339f8f549d7b701ea89b5ced8
cassiopeia <cassiopeia.neely@gmail.com>
2020-07-30 00:34:56 -0400

    categoryColors function
    » Created a function that will change the flashcards' color schemes based on their categories.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // On document ready, a random German word (and its plural form) will load in the German box. The German box color will correspond to the word's category.
-
 $(document).ready(function() {
     function getRandom(object) {
         let theKeys = Object.keys(object);
@@ -31,52 +30,33 @@ $(document).ready(function() {
             translation: 'apple',
             category: 'food'
         },
+        {
+            german: 'das Haus',
+            plural: 'die Häuser',
+            translation: 'house',
+            category: 'home'
+        }
     ];
     
     let randomWord = wordArray[getRandom(wordArray)];
     console.log(randomWord.german);
     $('#german-word').html(randomWord.german);
     $('#german-plural').html(randomWord.plural);
+
+    const categoryColors = () => {
+        if (randomWord.category === 'people') {
+            $('#german-box').css('backgroundColor', '#008b8b');
+            $('#german-word, #german-plural').css('color', '#f0f8ff');
+            $('#line').css('backgroundColor', '#ffd700');
+            $('.curly-braces').css('color', '#ffd700');
+        } else if (randomWord.category === 'food') {
+            $('#german-box').css('backgroundColor', '#f44336');
+            $('#german-word, #german-plural').css('color', '#f0f8ff');
+            $('#line').css('backgroundColor', '#ffd700');
+            $('.curly-braces').css('color', '#ffd700');
+        } else {
+
+        }
+    };
+    categoryColors();
 }); 
-
-
-
-
-
-// const source = document.getElementById('flashcards').innerHTML;
-
-// const template = Handlebars.compile(source);
-
-// const context = {
-//     wordArray: [
-//         {
-//             german: 'die Kartoffel',
-//             plural: 'die Kartoffeln',
-//             translation: 'potato',
-//             category: 'food'
-//         },
-//         {
-//             german: 'das Mädchen',
-//             plural: 'die Mädchen',
-//             translation: 'girl',
-//             category: 'people'
-//         },
-//         {
-//             german: 'der Junge',
-//             plural: 'die Jungen',
-//             translation: 'boy',
-//             category: 'people'
-//         },
-//         {
-//             german: 'der Apfel',
-//             plural: 'die Äpfel',
-//             translation: 'apple',
-//             category: 'food'
-//         },
-//     ]
-// };
-
-// const compiledHtml = template(context);
-
-// const flashcardText = document.getElementById('');
-// flashcardText.innerHTML = compiledHtml;
