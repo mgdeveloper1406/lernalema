af2c5b0dc973cc4acb9152e4a38e3697e20fceb1
cassiopeia <cassiopeia.neely@gmail.com>
2020-07-30 21:30:29 -0400

    click needs a fix
    » The random word generator works fine on initial page load, but I haven't figured out how to apply it to the click event.
    » Note: Originally did not WANT a button to click, but I was not able to figure out how to get the page to update automatically when the input value matches the correct translation. I made the input a required field, so the user can't move forward without adding text, but at this point, wrong answers are accepted if the user hits the enter key.

--- js/script.js
+++ js/script.js
@@ -1,62 +1,174 @@
 // On document ready, a random German word (and its plural form) will load in the German box. The German box color will correspond to the word's category.
-$(document).ready(function() {
-    function getRandom(object) {
-        let theKeys = Object.keys(object);
-        return theKeys[Math.floor(Math.random() * theKeys.length)];
-    }
+// $(document).ready(function() {
+//     function getRandom(object) {
+//         let theKeys = Object.keys(object);
+//         return theKeys[Math.floor(Math.random() * theKeys.length)];
+//     }
 
-    let wordArray = [
-        {
-            german: 'die Kartoffel',
-            plural: 'die Kartoffeln',
-            translation: 'potato',
-            category: 'food'
-        },
-        {
-            german: 'das Mädchen',
-            plural: 'die Mädchen',
-            translation: 'girl',
-            category: 'people'
-        },
-        {
-            german: 'der Junge',
-            plural: 'die Jungen',
-            translation: 'boy',
-            category: 'people'
-        },
-        {
-            german: 'der Apfel',
-            plural: 'die Äpfel',
-            translation: 'apple',
-            category: 'food'
-        },
-        {
-            german: 'das Haus',
-            plural: 'die Häuser',
-            translation: 'house',
-            category: 'home'
-        }
-    ];
+//     let wordArray = [
+//         {
+//             german: 'die Kartoffel',
+//             plural: 'die Kartoffeln',
+//             translation: 'potato',
+//             category: 'food'
+//         },
+//         {
+//             german: 'das Mädchen',
+//             plural: 'die Mädchen',
+//             translation: 'girl',
+//             category: 'people'
+//         },
+//         {
+//             german: 'der Junge',
+//             plural: 'die Jungen',
+//             translation: 'boy',
+//             category: 'people'
+//         },
+//         {
+//             german: 'der Apfel',
+//             plural: 'die Äpfel',
+//             translation: 'apple',
+//             category: 'food'
+//         },
+//         {
+//             german: 'das Haus',
+//             plural: 'die Häuser',
+//             translation: 'house',
+//             category: 'home'
+//         }
+//     ];
     
-    let randomWord = wordArray[getRandom(wordArray)];
-    console.log(randomWord.german);
+//     let randomWord = wordArray[getRandom(wordArray)];
+//     console.log(randomWord.german);
+//     $('#german-word').html(randomWord.german);
+//     $('#german-plural').html(randomWord.plural);
+
+//     const categoryColors = () => {
+//         if (randomWord.category === 'people') {
+//             $('#german-box').css('backgroundColor', '#008b8b');
+//             $('#german-word, #german-plural').css('color', '#f0f8ff');
+//             $('#line').css('backgroundColor', '#ffd700');
+//             $('.curly-braces').css('color', '#ffd700');
+//         } else if (randomWord.category === 'food') {
+//             $('#german-box').css('backgroundColor', '#f44336');
+//             $('#german-word, #german-plural').css('color', '#f0f8ff');
+//             $('#line').css('backgroundColor', '#ffd700');
+//             $('.curly-braces').css('color', '#ffd700');
+//         } else {
+
+//         }
+//     };
+//     categoryColors();
+
+//     // $('#your-translation').change(function() {
+//     //     if ($('#your-translation').val() = this.randomWord.translation) {
+//     //         $('#german-word').html(randomWord.german);
+//     //         $('#german-plural').html(randomWord.plural);
+//     //     } 
+//     // });
+// }); 
+
+
+let wordArray = [
+    {
+        german: 'die Kartoffel',
+        plural: 'die Kartoffeln',
+        translation: 'potato',
+        category: 'food'
+    },
+    {
+        german: 'das Mädchen',
+        plural: 'die Mädchen',
+        translation: 'girl',
+        category: 'people'
+    },
+    {
+        german: 'der Junge',
+        plural: 'die Jungen',
+        translation: 'boy',
+        category: 'people'
+    },
+    {
+        german: 'der Apfel',
+        plural: 'die Äpfel',
+        translation: 'apple',
+        category: 'food'
+    },
+    {
+        german: 'das Haus',
+        plural: 'die Häuser',
+        translation: 'house',
+        category: 'home'
+    }
+];
+
+function getRandom(object) {
+    let theKeys = Object.keys(object);
+    return theKeys[Math.floor(Math.random() * theKeys.length)];
+}
+
+let randomWord = wordArray[getRandom(wordArray)];
+console.log(randomWord.german);
+// $('#german-word').html(randomWord.german);
+// $('#german-plural').html(randomWord.plural);
+
+function updateWord() {
     $('#german-word').html(randomWord.german);
     $('#german-plural').html(randomWord.plural);
+}
+updateWord();
 
-    const categoryColors = () => {
-        if (randomWord.category === 'people') {
-            $('#german-box').css('backgroundColor', '#008b8b');
-            $('#german-word, #german-plural').css('color', '#f0f8ff');
-            $('#line').css('backgroundColor', '#ffd700');
-            $('.curly-braces').css('color', '#ffd700');
-        } else if (randomWord.category === 'food') {
-            $('#german-box').css('backgroundColor', '#f44336');
-            $('#german-word, #german-plural').css('color', '#f0f8ff');
-            $('#line').css('backgroundColor', '#ffd700');
-            $('.curly-braces').css('color', '#ffd700');
-        } else {
 
-        }
-    };
-    categoryColors();
-}); 
+
+
+const categoryColors = () => {
+    if (randomWord.category === 'people') {
+        $('#german-box').css('backgroundColor', '#008b8b');
+        $('#german-word, #german-plural').css('color', '#f0f8ff');
+        $('#line').css('backgroundColor', '#ffd700');
+        $('.curly-braces').css('color', '#ffd700');
+    } else if (randomWord.category === 'food') {
+        $('#german-box').css('backgroundColor', '#f44336');
+        $('#german-word, #german-plural').css('color', '#f0f8ff');
+        $('#line').css('backgroundColor', '#ffd700');
+        $('.curly-braces').css('color', '#ffd700');
+    } else {
+
+    }
+};
+categoryColors();
+
+$('#submit').on('click', function() {
+    let answer = $('#your-translation').val();
+    if (answer === randomWord.translation) {
+        return randomWord.next();
+        updateWord();
+        // $('#german-word').html(randomWord.german);
+        // $('#german-plural').html(randomWord.plural);
+    } else {
+        console.log(`Your answer was "${answer}"`);
+        console.log(`The correct translation is "${randomWord.translation}"`);
+    }
+});
+
+
+// $(document).keypress(function(event) {
+//     if (event.key === 'Enter') {
+//         let answer = $('#your-translation').val();
+//         let prompt = $('#german-word').val();
+//         console.log(prompt.translation);
+//         if (answer === prompt.translation) {
+//             $('#german-word').html(randomWord.german);
+//             $('#german-plural').html(randomWord.plural);
+//         } 
+//     }
+// });
+
+
+// let answer = $('#your-translation').val();
+// console.log(randomWord.translation);
+// console.log(answer);
+// if (answer === randomWord.translation) {
+//     $('#german-word').html(randomWord.german);
+//     $('#german-plural').html(randomWord.plural);
+// } 
