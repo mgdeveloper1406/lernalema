ca4b3946901feb01a45f97700d117482a10e99df
cassiopeia <cassiopeia.neely@gmail.com>
2020-08-05 20:24:19 -0400

    basically works!
    » At this point, the submit button now progresses the user forward, and the flashcard colors update accordingly.
    » SOLUTION: I think there were scope issues with the way I'd defined randomWord, so I moved that into the new gimmeWords() function. I also used .push() to move the "current" randomWord out of the way, in the click event, so the random word would have to change. Those words keep finding their way back into the mix, though, so I'd like to figure out why .slice() wasn't working before, and whether that would help solve the duplication problem.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,103 @@
+// this picks a random word from the word array
+function getRandom(object) {
+    let theKeys = Object.keys(object);
+    return theKeys[Math.floor(Math.random() * theKeys.length)];
+}
+
+// let randomWord = wordArray[getRandom(wordArray)];
+let randomWord;
+let wordArray;
+let usedWords = [];
+
+function gimmeWords() {
+    wordArray = [
+        {
+            german: 'die Kartoffel',
+            plural: 'die Kartoffeln',
+            translation: 'potato',
+            category: 'food'
+        },
+        {
+            german: 'das Mädchen',
+            plural: 'die Mädchen',
+            translation: 'girl',
+            category: 'people'
+        },
+        {
+            german: 'der Junge',
+            plural: 'die Jungen',
+            translation: 'boy',
+            category: 'people'
+        },
+        {
+            german: 'der Apfel',
+            plural: 'die Äpfel',
+            translation: 'apple',
+            category: 'food'
+        },
+        {
+            german: 'das Haus',
+            plural: 'die Häuser',
+            translation: 'house',
+            category: 'home'
+        }
+    ];
+
+    randomWord = wordArray[getRandom(wordArray)];
+    console.log(randomWord.german);
+
+    // this populates the flashcard words, when the document loads
+    $('#german-word').html(randomWord.german);
+    $('#german-plural').html(randomWord.plural);
+};
+
+gimmeWords();
+
+// this changes the flashcards' colors, based on the given word's category
+const categoryColors = () => {
+    if (randomWord.category === 'people') {
+        $('#german-box').css('backgroundColor', '#008b8b');
+        $('#german-word, #german-plural').css('color', '#f0f8ff');
+        $('#line').css('backgroundColor', '#ffd700');
+        $('.curly-braces').css('color', '#ffd700');
+    } else if (randomWord.category === 'food') {
+        $('#german-box').css('backgroundColor', '#f44336');
+        $('#german-word, #german-plural').css('color', '#f0f8ff');
+        $('#line').css('backgroundColor', '#ffd700');
+        $('.curly-braces').css('color', '#ffd700');
+    } 
+};
+
+categoryColors();
+
+// on clicking the submit button, the code below should check that the user's input matches the correct translation given in the word array. if so, the input field should clear, and the flashcard should progress on to the next random word. else, the console logs the input and the answer, to help move the user on, if needed. (NOTE: I also want to add a skip button.)
+$('#submit').on('click', function() {
+    let answer = $('#your-translation').val();
+    if (answer === randomWord.translation) { 
+        $('#your-translation').val('');
+        // randomWord = wordArray.splice(0, 1)[getRandom(wordArray)];
+        usedWords.push(randomWord);
+        gimmeWords();
+        categoryColors();
+        // $('#german-word').html(randomWord.german);
+        // $('#german-plural').html(randomWord.plural);
+    } else {
+        console.log(`Your answer was "${answer}"`);
+        console.log(`The correct translation is "${randomWord.translation}"`);
+    }
+});
+
+//--------------------------------------------------------------------
+
 // On document ready, a random German word (and its plural form) will load in the German box. The German box color will correspond to the word's category.
 // $(document).ready(function() {
 //     function getRandom(object) {
 //         let theKeys = Object.keys(object);
 //         return theKeys[Math.floor(Math.random() * theKeys.length)];
 //     }
+
+
+
 
 //     let wordArray = [
 //         {
@@ -69,82 +163,40 @@
 // }); 
 
 // the array of words
-let wordArray = [
-    {
-        german: 'die Kartoffel',
-        plural: 'die Kartoffeln',
-        translation: 'potato',
-        category: 'food'
-    },
-    {
-        german: 'das Mädchen',
-        plural: 'die Mädchen',
-        translation: 'girl',
-        category: 'people'
-    },
-    {
-        german: 'der Junge',
-        plural: 'die Jungen',
-        translation: 'boy',
-        category: 'people'
-    },
-    {
-        german: 'der Apfel',
-        plural: 'die Äpfel',
-        translation: 'apple',
-        category: 'food'
-    },
-    {
-        german: 'das Haus',
-        plural: 'die Häuser',
-        translation: 'house',
-        category: 'home'
-    }
-];
+// let wordArray = [
+//     {
+//         german: 'die Kartoffel',
+//         plural: 'die Kartoffeln',
+//         translation: 'potato',
+//         category: 'food'
+//     },
+//     {
+//         german: 'das Mädchen',
+//         plural: 'die Mädchen',
+//         translation: 'girl',
+//         category: 'people'
+//     },
+//     {
+//         german: 'der Junge',
+//         plural: 'die Jungen',
+//         translation: 'boy',
+//         category: 'people'
+//     },
+//     {
+//         german: 'der Apfel',
+//         plural: 'die Äpfel',
+//         translation: 'apple',
+//         category: 'food'
+//     },
+//     {
+//         german: 'das Haus',
+//         plural: 'die Häuser',
+//         translation: 'house',
+//         category: 'home'
+//     }
+// ];
 
-// this picks a random word from the word array
-function getRandom(object) {
-    let theKeys = Object.keys(object);
-    return theKeys[Math.floor(Math.random() * theKeys.length)];
-}
 
-let randomWord = wordArray[getRandom(wordArray)];
-console.log(randomWord.german);
-
-// this populates the flashcard words, when the document loads
-$('#german-word').html(randomWord.german);
-$('#german-plural').html(randomWord.plural);
-
-// this changes the flashcards' colors, based on the given word's category
-const categoryColors = () => {
-    if (randomWord.category === 'people') {
-        $('#german-box').css('backgroundColor', '#008b8b');
-        $('#german-word, #german-plural').css('color', '#f0f8ff');
-        $('#line').css('backgroundColor', '#ffd700');
-        $('.curly-braces').css('color', '#ffd700');
-    } else if (randomWord.category === 'food') {
-        $('#german-box').css('backgroundColor', '#f44336');
-        $('#german-word, #german-plural').css('color', '#f0f8ff');
-        $('#line').css('backgroundColor', '#ffd700');
-        $('.curly-braces').css('color', '#ffd700');
-    } 
-};
-
-categoryColors();
-
-// on clicking the submit button, the code below should check that the user's input matches the correct translation given in the word array. if so, the input field should clear, and the flashcard should progress on to the next random word. else, the console logs the input and the answer, to help move the user on, if needed. (NOTE: I also want to add a skip button.)
-$('#submit').on('click', function() {
-    let answer = $('#your-translation').val();
-    if (answer === randomWord.translation) { 
-        $('#your-translation').val('');
-        // randomWord = wordArray.splice(0, 1)[getRandom(wordArray)];
-        $('#german-word').html(randomWord.german);
-        $('#german-plural').html(randomWord.plural);
-    } else {
-        console.log(`Your answer was "${answer}"`);
-        console.log(`The correct translation is "${randomWord.translation}"`);
-    }
-});
 
 // $(document).keypress(function(event) {
 //     if (event.key === 'Enter') {
