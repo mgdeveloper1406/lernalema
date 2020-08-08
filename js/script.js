999e500e7833658fdc0053888f6e762a9180461c
cassiopeia <cassiopeia.neely@gmail.com>
2020-08-08 16:37:43 -0400

    array randomizes and reduces
    » The console logs showed that moveWord() was working, and I realized that wordArray was being reset once gimmeWords() was called in the click event. This is why the "popped" words kept coming back, so I removed the array from gimmeWords(). This originally threw an error that stated that the properties of the "undefined" randomWord could not be read, once the array was emptied, so I added a new if statement that will ignore gimmeWords() if the length of the array is less than zero.
    » The array now randomizes and removes each word that has been correctly translated. An alert lets the user know when they've reached the end of the deck of flashcards.

--- js/script.js
+++ js/script.js
@@ -1,78 +1,69 @@
+let randomWord;
+let wordArray;
+let i;
+
 // change the chameleon's color every time submit is clicked
 const chameleonColors = () => {
     $('#chameleon').attr('src', 'images/german-flash-03.svg');
 };
 
-// this picks a random word from the word array
+wordArray = [
+    {
+        german: 'die Kartoffel',
+        plural: 'die Kartoffeln',
+        translation: 'potato',
+        category: 'food'
+    },
+    {
+        german: 'das Mädchen',
+        plural: 'die Mädchen',
+        translation: 'girl',
+        category: 'people'
+    },
+    {
+        german: 'der Junge',
+        plural: 'die Jungen',
+        translation: 'boy',
+        category: 'people'
+    },
+    {
+        german: 'der Apfel',
+        plural: 'die Äpfel',
+        translation: 'apple',
+        category: 'food'
+    },
+    {
+        german: 'das Haus',
+        plural: 'die Häuser',
+        translation: 'house',
+        category: 'home'
+    },
+    {
+        german: 'der Schuh',
+        plural: 'die Schuhe',
+        translation: 'shoe',
+        category: 'home'
+    }
+];
+
+// this helps to pick a random word from the word array
 function getRandom(object) {
     let theKeys = Object.keys(object);
     return theKeys[Math.floor(Math.random() * theKeys.length)];
 }
 
-// let randomWord = wordArray[getRandom(wordArray)];
-let randomWord;
-let wordArray;
-let i;
-// let chameleonArray;
-// let flashIndex;
-// let usedWords = [];
-// let usedWords;
-
 function gimmeWords() {
-    wordArray = [
-        {
-            german: 'die Kartoffel',
-            plural: 'die Kartoffeln',
-            translation: 'potato',
-            category: 'food'
-        },
-        {
-            german: 'das Mädchen',
-            plural: 'die Mädchen',
-            translation: 'girl',
-            category: 'people'
-        },
-        {
-            german: 'der Junge',
-            plural: 'die Jungen',
-            translation: 'boy',
-            category: 'people'
-        },
-        {
-            german: 'der Apfel',
-            plural: 'die Äpfel',
-            translation: 'apple',
-            category: 'food'
-        },
-        {
-            german: 'das Haus',
-            plural: 'die Häuser',
-            translation: 'house',
-            category: 'home'
-        },
-        {
-            german: 'der Schuh',
-            plural: 'die Schuhe',
-            translation: 'shoe',
-            category: 'home'
-        },
-        // {}
-    ];
+    wordArray;
+    
+    if (wordArray.length > 0) {
+        // using the getRandom function, this assigns the value of the random word
+        randomWord = wordArray[getRandom(wordArray)];
+        console.log(randomWord.german);
 
-    randomWord = wordArray[getRandom(wordArray)];
-    console.log(randomWord.german);
-
-    // console.log(wordArray.indexOf(randomWord));
-
-    // this populates the flashcard words, when the document loads
-    $('#german-word').html(randomWord.german);
-    $('#german-plural').html(randomWord.plural);
-
-    // wordArray = wordArray.length - 1;
-
-    // console.log(i);
-    // wordArray.splice(i);
-    // console.log(wordArray);
+        // this populates the flashcard words, when the document loads
+        $('#german-word').html(randomWord.german);
+        $('#german-plural').html(randomWord.plural);
+    }
 };
 
 gimmeWords();
@@ -99,45 +90,21 @@ const categoryColors = () => {
 
 categoryColors();
 
-// function removeWord() {
-//     const i = wordArray.indexOf(randomWord);
-//     wordArray.splice(i, 1);
-//     wordArray.push(wordArray[i]);
-//     wordArray.pop();
-//     console.log(wordArray); 
-// };
-
 function moveWord() {
     i = wordArray.indexOf(randomWord);
     wordArray.push(wordArray.splice(wordArray.indexOf(wordArray[i]), 1)[0]);
-    // wordArray.pop(wordArray.push(wordArray.splice(wordArray.indexOf(wordArray[i]), 1)[0]));
-    // console.log(wordArray); 
 };
 
 // on clicking the submit button, the code below should check that the user's input matches the correct translation given in the word array. if so, the input field should clear, and the flashcard should progress on to the next random word. else, the console logs the input and the answer, to help move the user on, if needed. (NOTE: I also want to add a skip button.)
 $('#submit').on('click', function() {
     let answer = $('#your-translation').val();
+    
     if (answer === randomWord.translation) { 
         $('#your-translation').val('');
-        // usedWords.push(randomWord);
-        // console.log(usedWords);
-       
-        // wordArray.push(randomWord);
-        // wordArray.pop();
-
         moveWord();
-        console.log(wordArray);
-
+        wordArray.pop();
         gimmeWords();
         categoryColors();
-
-        // let usedIndex = wordArray.indexOf(this.randomWord);
-
-        // usedWords = wordArray.splice(usedIndex, 1);
-        // randomWord = wordArray.splice(0, 1)[getRandom(wordArray)];
-        // usedWords = wordArray.splice(0, 1);
-        // $('#german-word').html(randomWord.german);
-        // $('#german-plural').html(randomWord.plural);
     } else {
         console.log(`Your answer was "${answer}"`);
         console.log(`The correct translation is "${randomWord.translation}"`);
@@ -149,135 +116,3 @@ $('#submit').on('click', function() {
 
     chameleonColors();
 });
-
-//--------------------------------------------------------------------
-
-// On document ready, a random German word (and its plural form) will load in the German box. The German box color will correspond to the word's category.
-// $(document).ready(function() {
-//     function getRandom(object) {
-//         let theKeys = Object.keys(object);
-//         return theKeys[Math.floor(Math.random() * theKeys.length)];
-//     }
-
-
-
-
-//     let wordArray = [
-//         {
-//             german: 'die Kartoffel',
-//             plural: 'die Kartoffeln',
-//             translation: 'potato',
-//             category: 'food'
-//         },
-//         {
-//             german: 'das Mädchen',
-//             plural: 'die Mädchen',
-//             translation: 'girl',
-//             category: 'people'
-//         },
-//         {
-//             german: 'der Junge',
-//             plural: 'die Jungen',
-//             translation: 'boy',
-//             category: 'people'
-//         },
-//         {
-//             german: 'der Apfel',
-//             plural: 'die Äpfel',
-//             translation: 'apple',
-//             category: 'food'
-//         },
-//         {
-//             german: 'das Haus',
-//             plural: 'die Häuser',
-//             translation: 'house',
-//             category: 'home'
-//         }
-//     ];
-    
-//     let randomWord = wordArray[getRandom(wordArray)];
-//     console.log(randomWord.german);
-//     $('#german-word').html(randomWord.german);
-//     $('#german-plural').html(randomWord.plural);
-
-//     const categoryColors = () => {
-//         if (randomWord.category === 'people') {
-//             $('#german-box').css('backgroundColor', '#008b8b');
-//             $('#german-word, #german-plural').css('color', '#f0f8ff');
-//             $('#line').css('backgroundColor', '#ffd700');
-//             $('.curly-braces').css('color', '#ffd700');
-//         } else if (randomWord.category === 'food') {
-//             $('#german-box').css('backgroundColor', '#f44336');
-//             $('#german-word, #german-plural').css('color', '#f0f8ff');
-//             $('#line').css('backgroundColor', '#ffd700');
-//             $('.curly-braces').css('color', '#ffd700');
-//         } else {
-
-//         }
-//     };
-//     categoryColors();
-
-//     // $('#your-translation').change(function() {
-//     //     if ($('#your-translation').val() = this.randomWord.translation) {
-//     //         $('#german-word').html(randomWord.german);
-//     //         $('#german-plural').html(randomWord.plural);
-//     //     } 
-//     // });
-// }); 
-
-// the array of words
-// let wordArray = [
-//     {
-//         german: 'die Kartoffel',
-//         plural: 'die Kartoffeln',
-//         translation: 'potato',
-//         category: 'food'
-//     },
-//     {
-//         german: 'das Mädchen',
-//         plural: 'die Mädchen',
-//         translation: 'girl',
-//         category: 'people'
-//     },
-//     {
-//         german: 'der Junge',
-//         plural: 'die Jungen',
-//         translation: 'boy',
-//         category: 'people'
-//     },
-//     {
-//         german: 'der Apfel',
-//         plural: 'die Äpfel',
-//         translation: 'apple',
-//         category: 'food'
-//     },
-//     {
-//         german: 'das Haus',
-//         plural: 'die Häuser',
-//         translation: 'house',
-//         category: 'home'
-//     }
-// ];
-
-
-
-// $(document).keypress(function(event) {
-//     if (event.key === 'Enter') {
-//         let answer = $('#your-translation').val();
-//         let prompt = $('#german-word').val();
-//         console.log(prompt.translation);
-//         if (answer === prompt.translation) {
-//             $('#german-word').html(randomWord.german);
-//             $('#german-plural').html(randomWord.plural);
-//         } 
-//     }
-// });
-
-
-// let answer = $('#your-translation').val();
-// console.log(randomWord.translation);
-// console.log(answer);
-// if (answer === randomWord.translation) {
-//     $('#german-word').html(randomWord.german);
-//     $('#german-plural').html(randomWord.plural);
-// } 
